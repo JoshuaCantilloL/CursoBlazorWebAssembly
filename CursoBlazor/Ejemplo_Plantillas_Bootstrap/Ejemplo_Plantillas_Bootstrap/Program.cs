21080bd1a58f5dde3965a5407a24fc91e752ad04
Joshua Cantillo <jdcantillol@correo.udistrital.edu.co>
2025-02-26 09:41:47 -0500

    Avances proyecto Ventas

---
+++ CursoBlazor/Ejemplo_Plantillas_Bootstrap/Ejemplo_Plantillas_Bootstrap/Program.cs
@@ -0,0 +1,35 @@
+using Ejemplo_Plantillas_Bootstrap.Client.Pages;
+using Ejemplo_Plantillas_Bootstrap.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Ejemplo_Plantillas_Bootstrap.Client._Imports).Assembly);
+
+app.Run();
---
+++ CursoBlazor/Proyecto_Ventas.Shared/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ventas.Shared
+{
+    public class Venta
+    {
+        public int IdVenta  {get;set;}
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string NombreCliente { get; set; } = string.Empty;
+        public double? Precio { get; set; }
+        public double? DineroRecibido { get; set; }
+        public double? Cambio { get; set; }
+        public int Transferencia { get; set; } = 0;
+        
+
+
+    }
+}
